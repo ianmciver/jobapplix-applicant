535d7b3a2b4537fc1d50a7a5c090e1769094a315
tetondan <dfrehner1@gmail.com>
2019-06-25 02:03:46 -0600

    added script to redirect to https

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(function(req, res, next) {
+  if (req.get("Host") === "localhost:3000") {
+    next();
+  } else if (req.get("X-Forwarded-Proto") !== "https") {
+    console.log(req);
+    res.redirect("https://" + req.get("Host") + req.url);
+  } else next();
+});
+
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", function(req, res) {
